d04aa6375f49b7c784c91a7de54e2a41ff6a87fd
Patryk Zadykowicz <patrykzadykowiczali@gmail.com>
2025-05-21 02:04:14 +0200

    init commit

---
+++ src/main/java/wi/pb/clothesshop/ClothesshopApplication.java
@@ -0,0 +1,16 @@
+package wi.pb.clothesshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ClothesshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesshopApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/wi/pb/clothesshop/ClothesshopApplicationTests.java
@@ -0,0 +1,13 @@
+package wi.pb.clothesshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
