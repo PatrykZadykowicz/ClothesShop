dd5ecb8f25b6675d8a12a6888529a62ffde7ab41
larycho <sample@users.noreply.com>
2025-06-04 19:12:35 +0200

    Started work on OrderDao and OrderDaoImpl

---
+++ Spring Boot/src/main/java/wi/pb/clothesshop/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,60 @@
+package wi.pb.clothesshop.dao.impl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import wi.pb.clothesshop.dao.OrderDao;
+import wi.pb.clothesshop.entity.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class OrderDaoImpl implements OrderDao {
+
+    EntityManager entityManager;
+
+    @Autowired
+    public OrderDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<Order> findById(Long id) {
+        return Optional.ofNullable(entityManager.find(Order.class, id));
+    }
+
+    @Override
+    public List<Order> getAll() {
+        TypedQuery<Order> query = entityManager.createQuery("select o from Order o", Order.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    @Override
+    public Order save(Order order) { // TODO
+        entityManager.persist(order);
+        return order;
+    }
+
+    @Transactional
+    @Override
+    public Order update(Order order) { // TODO
+        return entityManager.merge(order);
+    }
+
+    @Transactional
+    @Override
+    public void delete(Order order) {
+        entityManager.remove(order);
+    }
+
+    @Override
+    public List<Order> findByUserId(Long userId) {
+        TypedQuery<Order> query = entityManager.createQuery("select o from Order o where user.id = :userid", Order.class);
+        query.setParameter("userid", userId);
+        return query.getResultList();
+    }
+}
