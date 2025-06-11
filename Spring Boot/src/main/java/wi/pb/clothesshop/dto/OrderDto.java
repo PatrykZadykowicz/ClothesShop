a563f4e326c7b4ccb743ddbc6b8339dd144f4c83
larycho <sample@users.noreply.com>
2025-06-11 14:20:48 +0200

    Added OrderItemDto and OrderDto classes

---
+++ Spring Boot/src/main/java/wi/pb/clothesshop/dto/OrderDto.java
@@ -0,0 +1,27 @@
+package wi.pb.clothesshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import wi.pb.clothesshop.enums.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDto {
+
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    private OrderStatus orderStatus;
+    private List<OrderItemDto> orderItems;
+
+    private Long userId;
+
+}
